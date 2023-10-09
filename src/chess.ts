350b6eb82fe8a5741178e7d84287ef3f2d9015eb
unknown <ajd93@njit.edu>
2023-10-09 13:07:09 -0400

    Header file working

--- src/chess.ts
+++ src/chess.ts
@@ -2,3 +2,5 @@ import { Chess } from "./chess_H";
 
 const chessBoard = new Chess.Board;
 chessBoard.printBoard();
+
+const test = new Chess.ChessPieces(Chess.PieceNames.Rook, Chess.BoardColors.White, Chess.Files.A, 0);
--- src/chess_H.ts
+++ src/chess_H.ts
@@ -1,5 +1,5 @@
 export module Chess {
-    export enum ChessPieces {
+    export enum PieceNames {
         Rook = "R",
         Knight = "N",
         Bishop = "B",
@@ -9,11 +9,37 @@ export module Chess {
         Empty = " ",
     }
     
-    enum BoardColors {
+    export enum BoardColors {
         White = "white",
         Black = "black",
+        NA = "none",
+    }
+
+    export enum Files {
+        A = 0,
+        B = 1,
+        C = 2,
+        D = 3,
+        E = 4,
+        F = 5,
+        G = 6, 
+        H = 7,
     }
     
+    export class ChessPieces {
+        public piece: PieceNames;
+        color: BoardColors;
+        file: Files;
+        rank: number;
+
+        constructor(piece: PieceNames, color: BoardColors, file: Files, rank: number) {
+            this.piece = piece;
+            this.color = color;
+            this.file = file;
+            this.rank = rank;
+        }
+    }
+
     let index: number = 0;
     let rotating: BoardColors = BoardColors.White;
     export class Tiles{
@@ -24,7 +50,6 @@ export module Chess {
         white_threatened: boolean;
         constructor(piece: ChessPieces) {
             if(index == 8) {
-                console.log(rotating);
                 index = 1;
                 this.color = rotating;
             } else {
@@ -32,7 +57,7 @@ export module Chess {
                 (rotating == BoardColors.White) ? rotating = BoardColors.Black : rotating = BoardColors.White;
                 index++;
             }
-            (piece == ChessPieces.Empty) ? this.empty = true : this.empty = false;
+            (piece.piece === PieceNames.Empty) ? this.empty = true : this.empty = false;
             this.piece = piece;
             this.black_threatened = false;
             this.white_threatened = false;
@@ -42,16 +67,16 @@ export module Chess {
     export class Board extends Tiles {
         board: Tiles[][];
         constructor() {
-            super(ChessPieces.Empty);
+            super(new ChessPieces(PieceNames.Empty, BoardColors.Black, Files.A, 10));
             this.board = [
-                [new Tiles(ChessPieces.Rook),new Tiles(ChessPieces.Knight),new Tiles(ChessPieces.Bishop),new Tiles(ChessPieces.Queen),new Tiles(ChessPieces.King),new Tiles(ChessPieces.Bishop),new Tiles(ChessPieces.Knight),new Tiles(ChessPieces.Rook)],
-                [new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn)],
-                [new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty)],
-                [new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty)],
-                [new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty)],
-                [new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty)],
-                [new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn)],
-                [new Tiles(ChessPieces.Rook),new Tiles(ChessPieces.Knight),new Tiles(ChessPieces.Bishop),new Tiles(ChessPieces.Queen),new Tiles(ChessPieces.King),new Tiles(ChessPieces.Bishop),new Tiles(ChessPieces.Knight),new Tiles(ChessPieces.Rook)],
+                [new Tiles(new ChessPieces(PieceNames.Rook, BoardColors.White, Files.A, 0)),new Tiles(new ChessPieces(PieceNames.Knight, BoardColors.White, Files.B, 0)),new Tiles(new ChessPieces(PieceNames.Bishop, BoardColors.White, Files.C, 0)),new Tiles(new ChessPieces(PieceNames.Queen, BoardColors.White, Files.D, 0)),new Tiles(new ChessPieces(PieceNames.King, BoardColors.White, Files.E, 0)),new Tiles(new ChessPieces(PieceNames.Bishop, BoardColors.White, Files.F, 0)),new Tiles(new ChessPieces(PieceNames.Knight, BoardColors.White, Files.G, 0)),new Tiles(new ChessPieces(PieceNames.Rook, BoardColors.White, Files.H, 0))],
+                [new Tiles(new ChessPieces(PieceNames.Pawn, BoardColors.White, Files.A, 1)),new Tiles(new ChessPieces(PieceNames.Pawn, BoardColors.White, Files.B, 1)),new Tiles(new ChessPieces(PieceNames.Pawn, BoardColors.White, Files.C, 1)),new Tiles(new ChessPieces(PieceNames.Pawn, BoardColors.White, Files.D, 1)),new Tiles(new ChessPieces(PieceNames.Pawn, BoardColors.White, Files.E, 1)),new Tiles(new ChessPieces(PieceNames.Pawn, BoardColors.White, Files.F, 1)),new Tiles(new ChessPieces(PieceNames.Pawn, BoardColors.White, Files.G, 1)),new Tiles(new ChessPieces(PieceNames.Pawn, BoardColors.White, Files.H, 1))],
+                [new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.A, 2)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.B, 2)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.C, 2)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.D, 2)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.E, 2)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.F, 2)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.G, 2)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.H, 2))],
+                [new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.A, 3)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.B, 3)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.C, 3)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.D, 3)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.E, 3)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.F, 3)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.G, 3)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.H, 3))],
+                [new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.A, 4)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.B, 4)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.C, 4)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.D, 4)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.E, 4)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.F, 4)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.G, 4)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.H, 4))],
+                [new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.A, 5)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.B, 5)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.C, 5)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.D, 5)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.E, 5)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.F, 5)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.G, 5)),new Tiles(new ChessPieces(PieceNames.Empty, BoardColors.NA, Files.H, 5))],
+                [new Tiles(new ChessPieces(PieceNames.Pawn, BoardColors.Black, Files.A, 6)),new Tiles(new ChessPieces(PieceNames.Pawn, BoardColors.Black, Files.B, 6)),new Tiles(new ChessPieces(PieceNames.Pawn, BoardColors.Black, Files.C, 6)),new Tiles(new ChessPieces(PieceNames.Pawn, BoardColors.Black, Files.D, 6)),new Tiles(new ChessPieces(PieceNames.Pawn, BoardColors.Black, Files.E, 6)),new Tiles(new ChessPieces(PieceNames.Pawn, BoardColors.Black, Files.F, 6)),new Tiles(new ChessPieces(PieceNames.Pawn, BoardColors.Black, Files.G, 6)),new Tiles(new ChessPieces(PieceNames.Pawn, BoardColors.Black, Files.H, 6))],
+                [new Tiles(new ChessPieces(PieceNames.Rook, BoardColors.Black, Files.A, 7)),new Tiles(new ChessPieces(PieceNames.Knight, BoardColors.Black, Files.B, 7)),new Tiles(new ChessPieces(PieceNames.Bishop, BoardColors.Black, Files.C, 7)),new Tiles(new ChessPieces(PieceNames.Queen, BoardColors.Black, Files.D, 7)),new Tiles(new ChessPieces(PieceNames.King, BoardColors.Black, Files.E, 7)),new Tiles(new ChessPieces(PieceNames.Bishop, BoardColors.Black, Files.F, 7)),new Tiles(new ChessPieces(PieceNames.Knight, BoardColors.Black, Files.G, 7)),new Tiles(new ChessPieces(PieceNames.Rook, BoardColors.Black, Files.H, 7))],
             ];
         }
 
@@ -70,3 +95,11 @@ export module Chess {
         }
     }
 }
+
+// const gameboard = new Chess.Board;
+// const board = gameboard.getBoard();
+// const rook = board[7][0].piece;
+// console.log(rook.piece);
+// console.log(rook.color);
+// console.log(rook.file);
+// console.log(rook.rank);
