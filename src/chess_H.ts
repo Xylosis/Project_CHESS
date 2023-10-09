658a1b3161b54681ddfbb0f9ef4d11f3c2983935
unknown <ajd93@njit.edu>
2023-10-09 01:27:25 -0400

    Added Tiles class, and made board made of tiles.

--- src/chess_H.ts
+++ src/chess_H.ts
@@ -9,39 +9,64 @@ export module Chess {
         Empty = " ",
     }
     
-    export class Board {
-        board: ChessPieces[][];
+    enum BoardColors {
+        White = "white",
+        Black = "black",
+    }
+    
+    let index: number = 0;
+    let rotating: BoardColors = BoardColors.White;
+    export class Tiles{
+        color: BoardColors;
+        empty: boolean;
+        piece: ChessPieces;
+        black_threatened: boolean;
+        white_threatened: boolean;
+        constructor(piece: ChessPieces) {
+            if(index == 8) {
+                console.log(rotating);
+                index = 1;
+                this.color = rotating;
+            } else {
+                this.color = rotating;
+                (rotating == BoardColors.White) ? rotating = BoardColors.Black : rotating = BoardColors.White;
+                index++;
+            }
+            (piece == ChessPieces.Empty) ? this.empty = true : this.empty = false;
+            this.piece = piece;
+            this.black_threatened = false;
+            this.white_threatened = false;
+        }
+    }
+
+    export class Board extends Tiles {
+        board: Tiles[][];
         constructor() {
+            super(ChessPieces.Empty);
             this.board = [
-                [ChessPieces.Rook,ChessPieces.Knight,ChessPieces.Bishop,ChessPieces.Queen,ChessPieces.King,ChessPieces.Bishop,ChessPieces.Knight,ChessPieces.Rook],
-                [ChessPieces.Pawn,ChessPieces.Pawn,ChessPieces.Pawn,ChessPieces.Pawn,ChessPieces.Pawn,ChessPieces.Pawn,ChessPieces.Pawn,ChessPieces.Pawn],
-                [ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty],
-                [ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty],
-                [ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty],
-                [ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty,ChessPieces.Empty],
-                [ChessPieces.Pawn,ChessPieces.Pawn,ChessPieces.Pawn,ChessPieces.Pawn,ChessPieces.Pawn,ChessPieces.Pawn,ChessPieces.Pawn,ChessPieces.Pawn],
-                [ChessPieces.Rook,ChessPieces.Knight,ChessPieces.Bishop,ChessPieces.Queen,ChessPieces.King,ChessPieces.Bishop,ChessPieces.Knight,ChessPieces.Rook],
+                [new Tiles(ChessPieces.Rook),new Tiles(ChessPieces.Knight),new Tiles(ChessPieces.Bishop),new Tiles(ChessPieces.Queen),new Tiles(ChessPieces.King),new Tiles(ChessPieces.Bishop),new Tiles(ChessPieces.Knight),new Tiles(ChessPieces.Rook)],
+                [new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn)],
+                [new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty)],
+                [new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty)],
+                [new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty)],
+                [new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty),new Tiles(ChessPieces.Empty)],
+                [new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn),new Tiles(ChessPieces.Pawn)],
+                [new Tiles(ChessPieces.Rook),new Tiles(ChessPieces.Knight),new Tiles(ChessPieces.Bishop),new Tiles(ChessPieces.Queen),new Tiles(ChessPieces.King),new Tiles(ChessPieces.Bishop),new Tiles(ChessPieces.Knight),new Tiles(ChessPieces.Rook)],
             ];
         }
 
-        public getBoard(): ChessPieces[][] {
+        public getBoard(): Tiles[][] {
             return this.board;
         }
 
         public printBoard(): void {
             for(let i: number = 0; i < 8; i++) {
-                const tempArray: ChessPieces[] = [];
+                const tempArray: BoardColors[] = [];
                 for(let j: number = 0; j < 8; j++) {
-                    tempArray.push(this.board[i][j]);
-                    //console.log(this.board[i][j]);
+                    tempArray.push(this.board[i][j].color);
                 }
                 console.log(tempArray.join(" "));
             }
         }
     }
-
-    // export class Tiles extends Board {
-
-    // }
 }
-
